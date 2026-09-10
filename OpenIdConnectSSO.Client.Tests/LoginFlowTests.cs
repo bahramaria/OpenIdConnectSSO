d80bbea1d18c31b78c3e53fe7e241a2bab4ae98a
Bahram Barazandeh <bahram.aria@gmail.com>
2026-09-10 14:37:52 +0330

    Fix LoginFlowTests controller action descriptor

--- OpenIdConnectSSO.Client.Tests/LoginFlowTests.cs
+++ OpenIdConnectSSO.Client.Tests/LoginFlowTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging.Abstractions;
 using OpenIdConnectSSO.Client.Controllers;
@@ -42,7 +42,7 @@ public class LoginFlowTests
             logger: NullLogger<AccountController>.Instance);
 
         controller.ControllerContext = new ControllerContext(
-            new ActionContext(httpContext, new RouteData(), new ActionDescriptor()));
+            new ActionContext(httpContext, new RouteData(), new ControllerActionDescriptor()));
 
         return controller;
     }
